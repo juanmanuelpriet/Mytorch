303e9dc076c81771fb12b59b91a731b3d767b82a
Juan <juanmanuelprietocorredor@gmail.com>
2026-03-01 17:46:14 -0500

    Fix: Export MSELoss in mytorch.nn to resolve ImportError in Example.ipynb.

--- mytorch/nn/__init__.py
+++ mytorch/nn/__init__.py
@@ -2,5 +2,5 @@ from .linear import Linear
 from .activation import *
 from .batchnorm import BatchNorm1d
 from .dropout import Dropout
-from .loss import CrossEntropyLoss
+from .loss import CrossEntropyLoss, MSELoss
 from .module import Sequential
